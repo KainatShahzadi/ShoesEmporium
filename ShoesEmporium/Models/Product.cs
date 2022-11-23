54eed777cbff99e335dd12b79dc3a48a3b6ed46f
kainatshahzadi866@gmail.com <kainatshahzadi866@gmail.com>
2022-11-23 16:17:16 +0500

    Add project files.

---
+++ ShoesEmporium/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ShoesEmporium.Models
+{
+    public class Product
+    {
+        [JsonPropertyName("id")]
+        public int Product_id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Product_name { get; set; }
+
+        [JsonPropertyName("image")]
+        public string Product_image { get; set; }
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize<Product>(this);
+        }
+    }
+}
---
+++ ShoesEmporium/Services/JasonProductFile.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using ShoesEmporium.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ShoesEmporium.Services
+{
+    public class JasonProductFile
+    {
+        public JasonProductFile(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        public string JsonFilePath
+        {
+            get
+            {
+                return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json");
+            }
+        }
+        public IEnumerable<Product> getProductsData()
+        {
+            using (var json_file = File.OpenText(JsonFilePath))
+            {
+                return JsonSerializer.Deserialize<Product[]>(json_file.ReadToEnd());
+            }
+        }
+
+
+
+
+    }
+}
